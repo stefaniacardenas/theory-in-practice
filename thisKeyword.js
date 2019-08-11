d3c91eb438a64f855246b194c07da14db10d9a23
Stefania Cardenas <stefania.cardenas@onfido.com>
2019-08-11 13:55:03 +0100

    More about scope and self

--- thisKeyword.js
+++ thisKeyword.js
@@ -14,3 +14,27 @@ const myObject = {
   }
 }
 myObject.c()
+
+// Scope and Self
+// this is the global scope
+color = 'blue';
+
+var myHair = {
+    color: "red",
+    logColor: function() {
+        // this a common practice in JS to debug scope related issues
+        var self = this;
+        console.log("In logColor - this.color: " + this.color); // will print red
+        console.log("In logColor - self.color: " + self.color); // will print red
+        (function() {
+            // `this` here has lost reference to the object
+            // because now `this` refers to the global scope as we are inside an iife
+            console.log("In IIFE - this.color: " + this.color); // will print blue
+            // self here is a variable that was defined inside the parent function scope
+            // so it will be available here and its value is the object myHair
+            console.log("In IIFE - self.color: " + self.color); // will print red
+        })();
+    }
+};
+
+myHair.logColor();
